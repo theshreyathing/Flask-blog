7b0a3cd3c63e6bc3dc40be40e8a4d5750c709781
Shreya Bhattacharya <64976753+theshreyathing@users.noreply.github.com>
2023-08-11 16:39:55 +0530

    Add files via upload

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app =  Flask(__name__, template_folder='template')
+
+app.config['SECRET_KEY'] = '1e8d826441423a20ed244252708dc3f4'  #Adding secret key to sheild from certain attacks
 
 posts = [
     {
@@ -26,6 +29,19 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title='About' )
+
+@app.route("/register", methods =['GET', 'POST']) 
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','Success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title= 'Register', form =form)
+
+@app.route("/login")   #route name
+def login(): #route function
+    form = LoginForm()
+    return render_template('login.html',title= 'Login', form =form)
 
 if __name__ == '__main__':
     app.run(debug=True)
